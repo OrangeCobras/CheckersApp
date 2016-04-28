4f221316105a68279be8eb3042f52d961c0a7b2b
Hendrikto <hendrik.to@gmail.com>
2016-04-28 17:19:24 +0200

    Add a Game activity and link it to the Menu activity.

--- app/src/main/java/com/orangecobras/checkersapp/Game.java
+++ app/src/main/java/com/orangecobras/checkersapp/Game.java
@@ -1,21 +1,19 @@
 package com.orangecobras.checkersapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Menu extends AppCompatActivity {
+public class Game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_game);
     }
 
-    public void startGame(View view) {
-        Intent intent = new Intent(this, Game.class);
-        startActivity(intent);
+    public void finish(View view) {
+        finish();
     }
 
 }
