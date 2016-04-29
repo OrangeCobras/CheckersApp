5a80a9495ccac0a02e76d79f421ebf5efc2b1169
Hendrikto <hendrik.to@gmail.com>
2016-04-29 14:42:32 +0200

    Resolve merge conflict.

--- app/src/main/java/com/orangecobras/checkersapp/Game.java
+++ app/src/main/java/com/orangecobras/checkersapp/Game.java
@@ -3,6 +3,7 @@ package com.orangecobras.checkersapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 
 import CheckersFramework.Board;
 import CheckersFramework.Status;
@@ -11,11 +12,13 @@ public class Game extends AppCompatActivity implements CheckersFramework.View {
 
     private Status status;
     private Board board;
+    private GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        gridView = (GridView) findViewById(R.id.gridViewGame);
     }
 
     public void finish(View view) {
