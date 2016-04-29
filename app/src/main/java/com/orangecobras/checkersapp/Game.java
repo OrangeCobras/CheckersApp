035e77a7131f71a6475452a9589fa555d170c433
Hendrikto <hendrik.to@gmail.com>
2016-04-29 14:06:26 +0200

    Resolve merge conflict.

--- app/src/main/java/com/orangecobras/checkersapp/Game.java
+++ app/src/main/java/com/orangecobras/checkersapp/Game.java
@@ -3,10 +3,13 @@ package com.orangecobras.checkersapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 
 import CheckersFramework.Board;
 import CheckersFramework.Status;
+import CheckersFramework.Point;
 
 public class Game extends AppCompatActivity implements CheckersFramework.View {
 
@@ -39,4 +42,34 @@ public class Game extends AppCompatActivity implements CheckersFramework.View {
     public void invalidate() {
         // TODO
     }
+}
+
+class BoardAdapter extends BaseAdapter {
+
+    private Board board;
+
+    public BoardAdapter(Board b) {
+        this.board = b;
+    }
+
+    @Override
+    public int getCount() {
+        return 100;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return board.getPiece(new Point(i % 9, i / 9));
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
 }
